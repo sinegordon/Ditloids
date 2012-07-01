af2ed54d3c41a75798958d98baf7ea404e1d46da
sinegordon <sinegordon@gmail.com>
2012-07-01 18:29:47 +0400

    Добавлены переходы между главным экраном и экраном выбора уровней

--- Ditloids/src/com/ditloids/MainActivity.java
+++ Ditloids/src/com/ditloids/MainActivity.java
@@ -1,25 +1,28 @@
 package com.ditloids;
 
-import com.ditloids.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener, OnTouchListener {
 	
-	/* Listener Example
-	public class Listener implements View.OnClickListener {
-
-    	public void onClick(View view) {
-        	((TextView)findViewById(R.id.widget33)).setText("fdefefef");
-    	}
-	}
-	*/
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         setContentView(R.layout.main);
+        Button startButton = (Button)findViewById(R.id.StartButton);
+        startButton.setOnClickListener(this);
+        startButton.setOnTouchListener(this);
+        //final LayoutInflater  inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //Button startButton = (Button)inflater.inflate(R.id.StartButton, null);
         /* Inflate Example
         final LayoutInflater  inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         TextView v = (TextView)inflater.inflate(R.layout.textview, null);
@@ -30,4 +33,28 @@ public class MainActivity extends Activity {
             v.append(lvl.GetDitloid(i)+"\n");
         */
     }
+
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+		switch (view.getId()) {
+	    case R.id.StartButton:
+	    	break;
+	    default:
+	    	break;
+	    }
+    	return false;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+	    case R.id.StartButton:
+	    	Intent intent = new Intent(this, LevelsActivity.class);
+	    	intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+	    	startActivity(intent);
+	    	break;
+	    default:
+	    	break;
+	    }	
+	}
 }
