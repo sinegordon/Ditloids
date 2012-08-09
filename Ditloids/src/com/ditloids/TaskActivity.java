34939cd91bc72be477e0b9e00bc24c3021ea4929
sinegordon <sinegordon@gmail.com>
2012-08-09 23:41:24 +0400

    Перделана логика действий при ответе

--- Ditloids/src/com/ditloids/TaskActivity.java
+++ Ditloids/src/com/ditloids/TaskActivity.java
@@ -37,13 +37,25 @@ public class TaskActivity extends Activity implements OnClickListener, OnKeyList
 		((TextView)findViewById(R.id.textView1)).setText("Уровень "+Integer.toString(currentLevel.GetLevelIndex()));
 		// Вставляем количество подсказок в надписи
 		((TextView)findViewById(R.id.textView2)).setText(Integer.toString(game.GetCountHints()));
-		// Заполняем поле дитлоида
-		((TextView)findViewById(R.id.textView3)).setText(currentLevel.GetDitloid(game.GetCurrentDitloidIndex()));
-		// Показываем клавиатуру
-		InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-		//imm.showSoftInput(findViewById(R.id.editText1), InputMethodManager.SHOW_FORCED);
-		imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+		// Если дитлоид не отгадан
+		if(!game.GetAnswer(game.GetCurrentDitloidIndex())){
+			// Заполняем поле дитлоида
+			((TextView)findViewById(R.id.textView3)).setText(currentLevel.GetDitloid(game.GetCurrentDitloidIndex()));
+			// Показываем клавиатуру
+			InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+			imm.showSoftInput(findViewById(R.id.editText1), InputMethodManager.SHOW_FORCED);
+		}
+		else{
+			// Заполняем поле дитлоида
+			((TextView)findViewById(R.id.textView3)).setText(currentLevel.GetDitloid(game.GetCurrentDitloidIndex()));	
+			// Выставляем правильный ответ в поле ввода
+			((EditText)findViewById(R.id.editText1)).setText(currentLevel.GetDitloidAnswer(game.GetCurrentDitloidIndex()));
+			// Перекрываем возможность редактирования поля ввода
+			((EditText)findViewById(R.id.editText1)).setEnabled(false);
+		}
+		//imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
 		// Устанавливаем обработчики событий
+		findViewById(R.id.buttonCheck).setOnClickListener(this);
 		findViewById(R.id.arrowButton).setOnClickListener(this);
 		findViewById(R.id.buttonHint).setOnClickListener(this);
 		findViewById(R.id.editText1).setOnKeyListener(this);
@@ -54,6 +66,7 @@ public class TaskActivity extends Activity implements OnClickListener, OnKeyList
 		switch (view.getId()) {
 	    case R.id.arrowButton:
 	    	// На экран уровня
+	    	TasksActivity.SetGame(game);
 	    	startActivity(new Intent(TaskActivity.this, TasksActivity.class));
 	    	finish();
 	    	break;
@@ -69,6 +82,39 @@ public class TaskActivity extends Activity implements OnClickListener, OnKeyList
 		    	((TextView)findViewById(R.id.textHint)).setText(game.GetCurrentLevel().GetDitloidHint(game.GetCurrentDitloidIndex()));
 	    	};
 	    	break;
+	    case R.id.buttonCheck:
+			// Если ответ верный
+			if(game.GetCurrentLevel().Verify(game.GetCurrentDitloidIndex(), ((EditText)findViewById(R.id.editText1)).getText().toString())){
+				// Перекрываем возможность редактирования поля ввода
+				((EditText)findViewById(R.id.editText1)).setEnabled(false);
+				// Убираем клавиатуру
+				InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(findViewById(R.id.editText1).getWindowToken(), 0);
+				// Меняем фон кнопки
+				findViewById(R.id.buttonCheck).setBackgroundResource(R.drawable.check_right);
+				// Меняем надпись на кнопке
+				((Button)findViewById(R.id.buttonCheck)).setText("Верно!");
+				// Показываем сообщение что верный ответ
+				//Toast.makeText(this, "Верно", Toast.LENGTH_LONG).show();
+				// Устанавливаем флаг верного ответа
+				game.SetAnswer(game.GetCurrentDitloidIndex(), true);
+				// Повышаем количество верных ответов
+				game.IncrementCountRight();
+				// Если это верный ответ кратный трем повышаем количество доступных подсказок
+				if(game.GetCountRight() % game.GetDivisor() == 0)
+					game.IncrementCountHints();
+				//TasksActivity.SetGame(game);
+				// На экран уровня
+		    	//startActivity(new Intent(TaskActivity.this, TasksActivity.class));
+		    	//finish();
+			}
+			else{
+				// Меняем фон кнопки
+				findViewById(R.id.buttonCheck).setBackgroundResource(R.drawable.check_wrong);
+				// Показываем сообщение что неверный ответ
+				//Toast.makeText(this, "Не верно", Toast.LENGTH_LONG).show();
+			};
+	    	break;
 	    default:
 	    	break;
 	    }
@@ -76,6 +122,7 @@ public class TaskActivity extends Activity implements OnClickListener, OnKeyList
 
 	@Override
 	public boolean onKey(View v, int keyCode, KeyEvent event) {
+		/*
 		// Если нажат Enter
 		if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
 			// Если ответ верный
@@ -100,9 +147,11 @@ public class TaskActivity extends Activity implements OnClickListener, OnKeyList
 			};
 			return true;
 		};
-		// Если нажата хардварная кнопка назад (вопрос будет ли работать ???)
+		*/
+		// Если нажата хардварная кнопка назад
 	    if (keyCode == KeyEvent.KEYCODE_BACK) {
 	    	// На экран уровня
+	    	TasksActivity.SetGame(game);
 	    	startActivity(new Intent(TaskActivity.this, TasksActivity.class));
 	    	finish();       
 	    } else {
