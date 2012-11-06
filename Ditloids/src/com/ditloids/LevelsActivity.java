40b20ccb6a0b768fc84e169cc12598e6aa3d70fd
sinegordon <sinegordon@gmail.com>
2012-11-06 11:31:49 +0300

    1.0

--- Ditloids/src/com/ditloids/LevelsActivity.java
+++ Ditloids/src/com/ditloids/LevelsActivity.java
@@ -1,80 +1,37 @@
 package com.ditloids;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.widget.Button;
-import android.widget.HorizontalScrollView;
 import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
-/**
- * A more complex demo including using a RadioGroup as "tabs" for the pager and showing the
- * dual-scrolling capabilities when a vertically scrollable element is nested inside the pager.
- */
 public class LevelsActivity extends Activity implements OnClickListener, OnKeyListener {
     private RadioGroup radioGroup = null;
-    // "Листатель"
-	private android.support.v4.view.ViewPager pager = null;
-	// Массив текстовых полей уровней
-	private TextView[] countViews = null;
-	// Массив кнопок перехода на уровни
-	private Button[] countButtons = null;
 	// Индекс текущего уровня на экране
-	private int checkedLevelIndex = 0;
+	private int checkedLevelIndex = 1;
 	private static Game game = null;
 	// Диалог
 	private AlertDialog.Builder adb = null;
-	List<View> pages = null;
-	         
+        
 
 	@Override
     public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.levels);
-		/*
-		LayoutInflater maininflater = LayoutInflater.from(this);
-		View levelspage = maininflater.inflate(R.layout.levels, null);
-        //setContentView(R.layout.levels);
-		LayoutInflater inflater = LayoutInflater.from(this);
-        pages = new ArrayList<View>();
-        View page = inflater.inflate(R.layout.pager_items, null);
-        for(int i = 1; i < game.GetCountLevels() + 1; i++){
-	    	int id = getResources().getIdentifier("rl" + Integer.toString(i), "id", getApplicationContext().getPackageName());
-	        RelativeLayout rl = (RelativeLayout) page.findViewById(id);
-	        pages.add(rl);
-        }
-        LevelsPagerAdapter pagerAdapter = new LevelsPagerAdapter(pages);
-        pager = new android.support.v4.view.ViewPager(getApplicationContext());
-        RelativeLayout rel = (RelativeLayout)findViewById(R.id.horizontal_pager);
-        rel.addView(pager);
-        pager.setAdapter(pagerAdapter);
-        pager.setCurrentItem(0);
-        */
-        
+        setContentView(R.layout.levels);
+    	Button but = (Button)findViewById(R.id.level_button);
         game.SetPauseMusic(false);
         this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        //radioGroup = (RadioGroup) findViewById(R.id.tabs);
+        radioGroup = (RadioGroup) findViewById(R.id.tabs);
 
         // Построение диалога
         adb = new AlertDialog.Builder(this);
@@ -83,25 +40,14 @@ public class LevelsActivity extends Activity implements OnClickListener, OnKeyLi
         // Кнопка положительного ответа диалога
         adb.setPositiveButton(R.string.yes, null);
         // Создаем диалог
-        adb.create();
-        countViews = new TextView[game.GetCountLevels()];
-        countButtons = new Button[game.GetCountLevels()];
-        
-        // Заполняем массивы кнопок уровней и надписей на уровнях
-        for(int i = 1; i < game.GetCountLevels() + 1; i++) {
-        	int id = getResources().getIdentifier("TextView" + Integer.toString(i), "id", getApplicationContext().getPackageName());
-        	TextView countView = (TextView)findViewById(id);
-        	int idb = getResources().getIdentifier("level" + Integer.toString(i) + "button", "id", getApplicationContext().getPackageName());
-        	Button but = (Button)findViewById(idb);
-        	countViews[i-1] = countView;
-        	countButtons[i-1] = but;
-            // Выставляем обработчики событий кнопок
-        	countButtons[i-1].setOnClickListener(LevelsActivity.this);
-        };
-        // Выставляем обработчик события кнопки
+        adb.create();   
+
+        // Выставляем обработчик события кнопок
+    	but.setOnClickListener(LevelsActivity.this);
         findViewById(R.id.arrowButton).setOnClickListener(this);
-        //radioGroup.setOnCheckedChangeListener(onCheckedChangedListener);
+        radioGroup.setOnCheckedChangeListener(onCheckedChangedListener);
         findViewById(R.id.arrowButton).bringToFront();
+        
     }
 	
     // Обработчик клика по радиобуттонам
@@ -110,12 +56,10 @@ public class LevelsActivity extends Activity implements OnClickListener, OnKeyLi
                 @Override
                 public void onCheckedChanged(final RadioGroup group, final int checkedId) {
                 	for(int i = 0; i < game.GetCountLevels(); i++){
-                		int id = getResources().getIdentifier("radio_btn_" + Integer.toString(i), "id", getApplicationContext().getPackageName());
+                		int id = getResources().getIdentifier("radio_btn_" + Integer.toString(i+1), "id", getApplicationContext().getPackageName());
                 		if (id == checkedId) {
-                			//pager.setCurrentItem(i);
-                			RelativeLayout rel = (RelativeLayout)findViewById(R.id.horizontal_pager);
-                			rel.addView(pages.get(i));
-                			checkedLevelIndex = i + 1;
+                			checkedLevelIndex = i+1;
+                			onResume();
                 			break;
                 		}
                 	}
@@ -163,18 +107,6 @@ public class LevelsActivity extends Activity implements OnClickListener, OnKeyLi
     	super.onConfigurationChanged(newConfig);  
 	}
 
-	/*@Override
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		// Если нажата хардварная кнопка назад
-	    if (event.getKeyCode() == KeyEvent.KEYCODE_BACK && event.getAction()==KeyEvent.ACTION_DOWN) {
-	    	// На главный экран
-	    	finish();
-			return true;
-	    } else {
-	        return false;
-	    }
-	}*/
-	
     // Пауза медиа-плеера при сворачивании приложения
     @Override
     protected void onPause() {
@@ -183,23 +115,24 @@ public class LevelsActivity extends Activity implements OnClickListener, OnKeyLi
     }
     
     // Снять паузу медиа-плеера при возврате на активность  
-    // Обновить отображение информации об уровнях при возврате на активность 
+    // Обновить отображение информации об уровне при возврате на активность 
     @Override
     protected void onResume() {
     	super.onResume();
     	game.SetPauseMusic(false);
+    	TextView countView = (TextView)findViewById(R.id.level_text_view);
+    	Button but = (Button)findViewById(R.id.level_button);
         // Выставляем правильные текст и картинки
-        for(int i = 1; i < game.GetCountLevels() + 1; i++) {
-        	countViews[i-1].setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Ukrainian-Play.ttf"));
-        	countViews[i-1].setText(Integer.toString(game.AnswersCount(i)) + " из " + Integer.toString(game.GetLevel(i).GetDitloidsCount()));
-        	if(!game.GetLevelAccess(i)) {
-        		countButtons[i-1].setBackgroundResource(R.drawable.level_lock);
-        	}
-        	else {
-        		int drawableId = getResources().getIdentifier("level" + Integer.toString(i), "drawable", getApplicationContext().getPackageName());
-        		countButtons[i-1].setBackgroundResource(drawableId);
-        	}
-        }
+    	countView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Ukrainian-Play.ttf"));
+    	if(!game.GetLevelAccess(checkedLevelIndex)) {
+    		countView.setText("Уровень " + Integer.toString(checkedLevelIndex));
+    		but.setBackgroundResource(R.drawable.level_lock);
+    	}
+    	else {
+    		int drawableId = getResources().getIdentifier("level" + Integer.toString(checkedLevelIndex), "drawable", getApplicationContext().getPackageName());
+    		countView.setText(Integer.toString(game.AnswersCount(checkedLevelIndex)) + " из " + Integer.toString(game.GetLevel(checkedLevelIndex).GetDitloidsCount()));
+        	but.setBackgroundResource(drawableId);
+    	}
     }
 
 	
@@ -210,7 +143,6 @@ public class LevelsActivity extends Activity implements OnClickListener, OnKeyLi
 
 	@Override
 	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
